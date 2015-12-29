8e0f6f023dd4cbfbe70507727bcc5586c33e5f39
John Sibert <sibert@hawaii.edu>
2015-12-28 14:01:57 -1000

    Johnz mods to catchMSY_graphics.R

--- R/catchMSY_graphics.R
+++ R/catchMSY_graphics.R
@@ -4,6 +4,10 @@ plot.stockID <- function(sID,.THEME=NULL){
 
 	with(sID,{
 		if(exists("idx")){
+			xinc = 50
+			yinc = 50
+			xpos = 2*xinc
+			ypos = yinc
 			# -----------------------------------------------------------------#
 			# Vulnerable biomass.
 			bio.df <- data.frame(Year=data$year,
@@ -18,11 +22,14 @@ plot.stockID <- function(sID,.THEME=NULL){
 			p <- p + stat_summary(geom="ribbon", fun.data="mean_cl_boot",
 			                      alpha=0.2,fill="red")
 			p <- p + labs(x="Year",y="Vulnerable Biomass (lbs)")
+ 			x11(title="Biomass",xpos=xpos,ypos=ypos)
 			print(p+.THEME)
 			# -----------------------------------------------------------------#
 
 			# -----------------------------------------------------------------#
 			#  Spawning biomass depletion
+			xpos = xpos + xinc
+			ypos = ypos + yinc
 			ssb.df <- data.frame(Year=data$year,
 			                     Biomass=t(ps.dt)[,idx])
 			ssb.df <- gather(ssb.df,key,dt,-Year)
@@ -35,16 +42,20 @@ plot.stockID <- function(sID,.THEME=NULL){
 			p <- p + stat_summary(geom="ribbon", fun.data="mean_cl_boot",
 			                      alpha=0.2,fill="red")
 			p <- p + labs(x="Year",y="Spawning Biomass Depletion")
+ 			x11(title="Depletion",xpos=xpos,ypos=ypos)
 			print(p+.THEME)
 			# -----------------------------------------------------------------#
 
 			# -----------------------------------------------------------------#
 			# Parameter distributions
+			xpos = xpos + xinc
+			ypos = ypos + yinc
 			gS <- gather(as.data.frame(S[idx,]))
 			p <- ggplot(gS,aes(x=value)) 
 			p <- p + geom_histogram(aes(y=..density..),alpha=0.3) 
 			p <- p + geom_density(col=NA,fill="red",alpha=0.2) 
 			p <- p + facet_wrap(~key,scales="free")
+ 			x11(title="Parameters",xpos=xpos,ypos=ypos)
 			print(p+.THEME)
 			# -----------------------------------------------------------------#
 
